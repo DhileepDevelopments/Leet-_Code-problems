c09f11dadac3468c587a861377dbbf050d1b99ef
dhileep <146449442+DhileepDevelopments@users.noreply.github.com>
2025-12-08 16:20:04 +0530

    Time: 5 ms (15.10%) | Memory: 43.1 MB (42.23%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,43 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack=new Stack<>();
+//         for(char ch:s.toCharArray()){
+//             if(ch == '['||ch=='{'||ch=='('){
+//                 stack.push(ch);
+//             }else if(ch==')'||ch==']'||ch=='}'){
+//                 if(stack.isEmpty()){
+//                     return false;
+//                 }
+//                 char top=stack.pop();
+//                 if(ch==')'&&top!='('||
+//                     ch=='}'&&top!='{'||
+//                     ch==']'&&top!='['){
+//                         return false;
+//                     }
+//             }
+//         }
+//         return stack.isEmpty();
+        
+//     }
+// }
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch == '['||ch=='{'||ch=='('){
-                stack.push(ch);
-            }else if(ch==')'||ch==']'||ch=='}'){
-                if(stack.isEmpty()){
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
                     return false;
                 }
-                char top=stack.pop();
-                if(ch==')'&&top!='('||
-                    ch=='}'&&top!='{'||
-                    ch==']'&&top!='['){
-                        return false;
-                    }
             }
         }
-        return stack.isEmpty();
-        
+
+        return stack.isEmpty();        
     }
 }
